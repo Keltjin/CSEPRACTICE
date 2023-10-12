b1a31d6c85f84e5ae8777b29ca8900e46a7349d0
David Gjennestad <davigjenn@gmail.com>
2023-10-12 14:37:49 -0600

    Finished Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string playAgain;
+        
+        do 
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 100);
+
+            int guess;
+            int tracker = 0;
+
+            do
+            {
+                Console.Write("What is your guess? ");
+                string userGuess = Console.ReadLine();
+                guess = int.Parse(userGuess);
+
+                tracker++;
+
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            } while (guess != magicNumber);
+
+            Console.WriteLine($"It took you {tracker} times to guess the number.");
+            
+            Console.Write("Would you like to play again? ");
+            playAgain = Console.ReadLine();
+
+        } while (playAgain != "no");
+
     }
-}
+}   
+
