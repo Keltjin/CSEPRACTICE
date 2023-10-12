8401e7f6c72029d4bb8f83799e39674c48b9b9a9
David Gjennestad <davigjenn@gmail.com>
2023-10-12 14:34:49 -0600

    Finished Prep3 and 4.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,61 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        string userNumber;
+        int number;
+        int sum = 0;
+        int max = -999999999;
+        int smallPositive = 0;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do 
+        {
+            Console.Write("Enter number: ");
+            userNumber = Console.ReadLine();
+            number = int.Parse(userNumber);
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+                if (number > max)
+                {
+                    max = number;
+                }
+
+                if (number > 0)
+                {
+                    smallPositive = number;
+                }
+            }
+
+        } while (number != 0);
+
+        foreach (int n in numbers)
+        {
+            sum += n;
+        }
+
+        numbers.Sort();
+
+        float newSum = Convert.ToSingle(sum);
+
+        float average = newSum / numbers.Count;
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
+        Console.WriteLine($"The smallest positive number is: {smallPositive}");
+        Console.WriteLine($"The sorted list is:");
+
+        foreach (int sortNumber in numbers)
+        {
+            Console.WriteLine(sortNumber);
+        }
     }
 }
